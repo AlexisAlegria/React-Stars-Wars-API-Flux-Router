a191aba753aa18ae0935f82d464d35fbc85eafbe
Alexis Alegria <alexisalegriav@gmail.com>
2021-10-21 18:22:01 -0300

    fixing Spacing and Layout of Navbar elements

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,63 +1,62 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import starsWarsImage from "../../img/stars-wars-title.png";
 
 export const Navbar = () => {
 	const { store, actions } = useContext(Context);
 	return (
-		<nav className="navbar navbar-dark bg-dark mb-3 px-5">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">
-					<img
-						className="logosw w-125"
-						// src="https://upload.wikimedia.org/wikipedia/commons/c/ce/Star_wars2.svg"
-						src="https://static.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg"
-					/>
-				</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/personajes">
-					<button className="btn btn-light mx-3">Characters</button>
-				</Link>
-				<Link to="/vehiculos">
-					<button className="btn btn-light mx-3">Vehicles</button>
-				</Link>
-				<Link to="/planetas">
-					<button className="btn btn-light mx-3">Planets</button>
-				</Link>
-				<Link className="dropdown">
-					<button
-						className="btn btn-warning dropdown-toggle"
-						type="button"
-						id="dropdownMenuButton1"
-						data-bs-toggle="dropdown"
-						aria-haspopup="true"
-						aria-expanded="false">
-						Favorites {store.favoritesList.length}
-					</button>
-					<ul className="dropdown-menu  dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
-						{store.favoritesList.map((item, index) => {
-							return (
-								<li
-									key={index}
-									className="list-group-item list-group-item-action d-flex justify-content-between">
-									{item}
-									<span>
-										<a>
-											<i
-												onClick={() => {
-													actions.setEliminarFavoritos(item);
-												}}
-												className="far fa-trash-alt"
-											/>
-										</a>
-									</span>
-								</li>
-							);
-						})}
-					</ul>
-				</Link>
-			</div>
-		</nav>
+		<div className="container">
+			<nav className="navbar navbar-dark bg-dark mb-3 px-1 justify-content-between">
+				<div className="col-3">
+					<Link to="/">
+						<img className="logosw w-75 ms-3" src={starsWarsImage} />
+					</Link>
+				</div>
+				<div className="col-auto">
+					<Link to="/personajes">
+						<button className="btn btn-light mx-3">Characters</button>
+					</Link>
+					<Link to="/vehiculos">
+						<button className="btn btn-light mx-3">Vehicles</button>
+					</Link>
+					<Link to="/planetas">
+						<button className="btn btn-light mx-3">Planets</button>
+					</Link>
+					<Link className="dropdown">
+						<button
+							className="btn btn-warning dropdown-toggle mx-3"
+							type="button"
+							id="dropdownMenuButton1"
+							data-bs-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+							Favorites {store.favoritesList.length}
+						</button>
+						<ul className="dropdown-menu  dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
+							{store.favoritesList.map((item, index) => {
+								return (
+									<li
+										key={index}
+										className="list-group-item list-group-item-action d-flex justify-content-between">
+										{item}
+										<span>
+											<a>
+												<i
+													onClick={() => {
+														actions.setEliminarFavoritos(item);
+													}}
+													className="far fa-trash-alt"
+												/>
+											</a>
+										</span>
+									</li>
+								);
+							})}
+						</ul>
+					</Link>
+				</div>
+			</nav>
+		</div>
 	);
 };
